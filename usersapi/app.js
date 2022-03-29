2c8de093784eeac154b0ffa61d079eec48c07411
prilika1 <53357004+prilika1@users.noreply.github.com>
2022-03-28 19:50:46 -0600

    CORS reworked

--- usersapi/app.js
+++ usersapi/app.js
@@ -2,10 +2,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var studentsRouter = require('./routes/students');
 
 var app = express();
+
+var corsOptions = {
+    origin: process.env.ORIGIN_ADDRESS,
+    optionsSuccessStatus: 200
+}
+app.use(cors())
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -13,11 +20,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function (req, res, next) {
+/*app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-});
+});*/
 app.use('/users', studentsRouter);
 
 
